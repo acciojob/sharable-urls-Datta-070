01e8a1aaaf48df7d7d9f43d892bfe0d0681b1322
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-05 11:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// your code
 // your code here
+document.addEventListener("DOMContentLoaded", function() {
+      var form = document.getElementById("myForm");
+      form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        updateURL();
+      });
+    });
+
+    function updateURL() {
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+      document.getElementById("url").textContent = url;
+	}
